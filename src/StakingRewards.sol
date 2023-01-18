2d860b10d66b0b2af5fdbb579a5d25d54a34e19e
Sunur Efe Vural <efe@efe.kim>
2023-01-18 16:30:39 +0300

    use two contracts for staking and funding

--- src/StakingRewards.sol
+++ src/StakingRewards.sol
@@ -1,7 +1,6 @@
 pragma solidity 0.8.17;
 
-import "openzeppelin/access/AccessControl.sol";
-import "openzeppelin/token/ERC20/IERC20.sol";
+import "./StakingRewardsFunding.sol";
 
 /**
  * @title Staking Rewards Single Token
@@ -10,14 +9,13 @@ import "openzeppelin/token/ERC20/IERC20.sol";
  * @dev For funding the contract, use `addReward()`. DO NOT DIRECTLY SEND TOKENS TO THE CONTRACT!
  * @dev Limitations (checked through input sanitization):
  *        1) The sum of all tokens added through `addReward()` cannot exceed `2**96-1`,
- *        2) A user's staked balance cannot exceed `2**96-1`.
+ *        2) Total staked balance cannot exceed `2**96-1`.
  * @dev Assumptions (not checked, assumed to be always true):
- *        1) `block.timestamp < 2**64 - 2**32`,
- *        2) rewardToken returns false or reverts on failing transfers,
- *        3) Number of users does not exceed `(2**256-1)/(2**96-1)`.
+ *        1) `block.timestamp < 2**40`,
+ *        2) rewardToken returns false or reverts on failing transfers.
  * @author shung
  */
-contract StakingRewards is AccessControl {
+contract StakingRewards is StakingRewardsFunding {
     struct User {
         uint160 rewardPerTokenPaid;
         uint96 balance;
@@ -26,127 +24,44 @@ contract StakingRewards is AccessControl {
     mapping(address => User) public users;
 
     uint160 public rewardPerTokenStored;
-    uint96 public lastUpdate;
-
-    uint96 public rewardRate;
-    uint64 public periodFinish;
-    uint96 public totalRewardAdded;
-
-    uint256 public totalStaked = 0;
-    uint256 public periodDuration = 1 days;
-
-    uint256 private constant PRECISION = 2**64-1;
-    uint256 private constant MAX_ADDED = 2**96-1;
-    uint256 private constant MAX_PERIOD = 2**32-1;
-    uint256 private constant MAX_BALANCE = 2**96-1;
-
-    bytes32 private constant FUNDER_ROLE = keccak256("FUNDER_ROLE");
-    bytes32 private constant DURATION_ROLE = keccak256("DURATION_ROLE");
-
-    IERC20 public immutable rewardToken;
+    uint96 public totalStaked = 0;
 
     event Staked(address indexed user, uint256 indexed amount, uint256 rewards);
     event Withdrawn(address indexed user, uint256 indexed amount, uint256 reward);
-    event RewardAdded(uint256 reward);
-    event PeriodDurationUpdated(uint256 newDuration);
 
-    error InvalidAmount(uint256 amount);
-    error InvalidDuration(uint256 duration);
-    error BalanceOverflow(uint256 balance);
-    error DistributionOverflow(uint256 distributed);
-    error NoReward();
-    error FailedTransfer();
-    error OngoingPeriod();
+    error InvalidWithdrawAmount(uint256 amountToWithdraw, uint256 existingBalance);
+    error InvalidStakeAmount(uint256 amountToStake);
 
-    modifier updateRewardPerTokenStored() {
+    constructor(address newRewardsToken, address newAdmin)
+        StakingRewardsFunding(newRewardsToken, newAdmin)
+    {}
+
+    function stake(uint256 amount) external {
         unchecked {
-            uint256 tmpTotalStaked = totalStaked;
-            if (tmpTotalStaked != 0) {
-                rewardPerTokenStored += uint160((_pendingRewards() * PRECISION) / tmpTotalStaked);
-            }
-            lastUpdate = uint96(block.timestamp);
-        }
-        _;
-    }
-
-    constructor(address newRewardToken, address newAdmin) {
-        rewardToken = IERC20(newRewardToken);
-        _grantRole(DEFAULT_ADMIN_ROLE, newAdmin);
-        _grantRole(FUNDER_ROLE, newAdmin);
-        _grantRole(DURATION_ROLE, newAdmin);
-    }
-
-    function stake(uint256 amount) external updateRewardPerTokenStored {
-        unchecked {
-            if (amount > MAX_BALANCE) revert InvalidAmount(amount);
+            if (amount > MAX_TOKEN) revert InvalidStakeAmount(amount);
             User storage user = users[msg.sender];
-            uint256 oldBalance = user.balance;
-            uint160 rewardPerToken = rewardPerTokenStored;
-            uint256 rewardPerTokenPayable = rewardPerToken - user.rewardPerTokenPaid;
-            uint256 reward = (oldBalance * rewardPerTokenPayable) / PRECISION;
+            uint256 reward = _updateRewardPerTokenStored(user);
             uint256 totalAmount = amount + reward;
-            uint256 newBalance = oldBalance + totalAmount;
-            if (newBalance > MAX_BALANCE) revert BalanceOverflow(newBalance);
-            totalStaked += totalAmount;
-            user.balance = uint96(newBalance);
-            user.rewardPerTokenPaid = uint160(rewardPerToken);
-            if (!rewardToken.transferFrom(msg.sender, address(this), amount)) {
-                revert FailedTransfer();
-            }
+            uint256 newTotalStaked = totalStaked + totalAmount;
+            if (newTotalStaked > MAX_TOKEN) revert InvalidStakeAmount(amount);
+            totalStaked = uint96(newTotalStaked);
+            user.balance += uint96(totalAmount);
+            _transferFromCaller(amount);
             emit Staked(msg.sender, amount, reward);
         }
     }
 
-    function withdraw(uint256 amount) external updateRewardPerTokenStored {
+    function withdraw(uint256 amount) external {
         unchecked {
             User storage user = users[msg.sender];
-            uint256 oldBalance = user.balance;
-            if (amount > oldBalance) revert InvalidAmount(amount);
-            uint160 rewardPerToken = rewardPerTokenStored;
-            uint256 rewardPerTokenPayable = rewardPerToken - user.rewardPerTokenPaid;
-            uint256 reward = (oldBalance * rewardPerTokenPayable) / PRECISION;
-            totalStaked -= amount;
-            user.balance = uint96(oldBalance - amount);
-            user.rewardPerTokenPaid = rewardPerToken;
-            if (!rewardToken.transfer(msg.sender, amount + reward)) revert FailedTransfer();
+            uint256 reward = _updateRewardPerTokenStored(user);
+            uint256 userOldBalance = user.balance;
+            if (amount > userOldBalance) revert InvalidWithdrawAmount(amount, userOldBalance);
+            totalStaked -= uint96(amount);
+            user.balance = uint96(userOldBalance - amount);
+            _transferToCaller(amount + reward);
             emit Withdrawn(msg.sender, amount, reward);
         }
-    }
-
-    function addReward(uint256 amount)
-        external
-        onlyRole(FUNDER_ROLE)
-        updateRewardPerTokenStored
-    {
-        unchecked {
-            uint256 tmpPeriodFinish = periodFinish;
-            uint256 tmpPeriodDuration = periodDuration;
-            if (amount == 0 || amount > MAX_ADDED) revert InvalidAmount(amount);
-            uint256 newTotalRewardAdded = totalRewardAdded + amount;
-            if (newTotalRewardAdded > MAX_ADDED) revert DistributionOverflow(newTotalRewardAdded);
-            totalRewardAdded = uint96(newTotalRewardAdded);
-            if (block.timestamp >= tmpPeriodFinish) {
-                rewardRate = uint96(amount / tmpPeriodDuration);
-            } else {
-                uint256 leftover = (tmpPeriodFinish - block.timestamp) * rewardRate;
-                rewardRate = uint96((amount + leftover) / tmpPeriodDuration);
-            }
-            periodFinish = uint64(block.timestamp + tmpPeriodDuration);
-            if (!rewardToken.transferFrom(msg.sender, address(this), amount)) {
-                revert FailedTransfer();
-            }
-            emit RewardAdded(amount);
-        }
-    }
-
-    function setPeriodDuration(uint256 newDuration)
-        external
-        onlyRole(DURATION_ROLE)
-    {
-        if (periodFinish >= block.timestamp) revert OngoingPeriod();
-        if (newDuration == 0 || newDuration > MAX_PERIOD) revert InvalidDuration(newDuration);
-        periodDuration = newDuration;
-        emit PeriodDurationUpdated(newDuration);
     }
 
     function earned(address account) external view returns (uint256) {
@@ -160,17 +75,16 @@ contract StakingRewards is AccessControl {
         }
     }
 
-    function _pendingRewards() private view returns (uint256) {
+    function _updateRewardPerTokenStored(User storage user) private returns (uint256 reward) {
         unchecked {
-            uint256 tmpPeriodFinish = periodFinish;
-            uint256 tmpLastUpdate = lastUpdate;
-            uint256 lastTimeRewardApplicable = tmpPeriodFinish < block.timestamp
-                ? tmpPeriodFinish
-                : block.timestamp;
-            uint256 duration = lastTimeRewardApplicable > tmpLastUpdate
-                ? lastTimeRewardApplicable - tmpLastUpdate
-                : 0;
-            return duration * rewardRate;
+            uint160 rewardPerToken = rewardPerTokenStored;
+            if (totalStaked != 0) {
+                rewardPerToken += uint160(_claim() * PRECISION / totalStaked);
+                rewardPerTokenStored = rewardPerToken;
+            }
+            user.rewardPerTokenPaid = rewardPerToken;
+            uint256 rewardPerTokenPayable = rewardPerToken - user.rewardPerTokenPaid;
+            reward = uint160((user.balance * rewardPerTokenPayable) / PRECISION);
         }
     }
 }
---
+++ src/StakingRewardsFunding.sol
@@ -0,0 +1,151 @@
+// SPDX-License-Identifier: GPLv3
+pragma solidity 0.8.17;
+
+import "openzeppelin/access/AccessControlEnumerable.sol";
+import "openzeppelin/token/ERC20/IERC20.sol";
+
+/**
+ * @title Staking Rewards Funding
+ * @author Shung for Pangolin
+ * @notice A contract that is only the rewards part of `StakingRewards`.
+ * @dev The inheriting contract must call `_claim()` to check its reward since the last time the
+ *      same call was made. Then, based on the reward amount, the inheriting contract shall
+ *      determine the distribution to stakers. The purpose of this architecture is to separate the
+ *      logic of funding from the staking and reward distribution.
+ */
+abstract contract StakingRewardsFunding is AccessControlEnumerable {
+    uint80 private _rewardRate;
+    uint40 private _lastUpdate;
+    uint40 private _periodFinish;
+    uint96 private _totalRewardAdded;
+    uint256 public periodDuration = 1 days;
+    IERC20 public immutable rewardsToken;
+    bytes32 public constant FUNDER_ROLE = keccak256("FUNDER_ROLE");
+    uint256 internal constant PRECISION = type(uint64).max;
+    uint256 internal constant MAX_TOKEN = type(uint96).max;
+    uint256 internal constant MIN_PERIOD_DURATION = 2 ** 16 + 1;
+    uint256 internal constant MAX_PERIOD_DURATION = type(uint32).max;
+
+    event PeriodManuallyEnded(uint256 originalEndTime);
+    event RewardAdded(uint256 reward);
+    event PeriodDurationUpdated(uint256 newDuration);
+
+    error InvalidNewAdmin(address newAdmin);
+    error InvalidRewardsToken(address newRewardsToken);
+    error ShortPeriodDuration(uint256 newDuration);
+    error LongPeriodDuration(uint256 newDuration);
+    error OngoingPeriod(uint256 currentPeriodFinish);
+    error FinishedPeriod(uint256 lastPeriodFinish);
+    error InvalidRewardAmount(uint256 addedReward);
+    error InvalidRewardRate(uint256 newRewardRate);
+    error FailedTransferTo(address recipient, uint256 amount);
+    error FailedTransferFrom(address sender, uint256 amount);
+
+    constructor(address newRewardsToken, address newAdmin) {
+        if (newAdmin == address(0)) revert InvalidNewAdmin(newAdmin);
+        if (newRewardsToken.code.length == 0) revert InvalidRewardsToken(newRewardsToken);
+        rewardsToken = IERC20(newRewardsToken);
+        _grantRole(DEFAULT_ADMIN_ROLE, newAdmin);
+        _grantRole(FUNDER_ROLE, newAdmin);
+    }
+
+    function setPeriodDuration(uint256 newDuration) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        uint256 tmpPeriodFinish = _periodFinish;
+        if (tmpPeriodFinish > block.timestamp) revert OngoingPeriod(tmpPeriodFinish);
+        if (newDuration < MIN_PERIOD_DURATION) revert ShortPeriodDuration(newDuration);
+        if (newDuration > MAX_PERIOD_DURATION) revert LongPeriodDuration(newDuration);
+        periodDuration = newDuration;
+        emit PeriodDurationUpdated(newDuration);
+    }
+
+    function endPeriod() external onlyRole(DEFAULT_ADMIN_ROLE) {
+        uint256 tmpPeriodFinish = _periodFinish;
+        if (block.timestamp >= tmpPeriodFinish) revert FinishedPeriod(tmpPeriodFinish);
+        unchecked {
+            uint256 leftover = (tmpPeriodFinish - block.timestamp) * _rewardRate;
+            _totalRewardAdded -= uint96(leftover);
+            _periodFinish = uint40(block.timestamp);
+            _transferToCaller(leftover);
+            emit PeriodManuallyEnded(tmpPeriodFinish);
+        }
+    }
+
+    function addReward(uint256 amount) external onlyRole(FUNDER_ROLE) {
+        uint256 tmpPeriodDuration = periodDuration;
+        if (amount > MAX_TOKEN) revert InvalidRewardAmount(amount);
+        _totalRewardAdded += uint96(amount);
+        // Update the _rewardRate, ensuring leftover rewards from the ongoing period are included.
+        // Note that we are using `_lastUpdate` instead of `block.timestamp`, otherwise we would
+        // have to “stash” the rewards from `_lastUpdate` to `block.timestamp` in storage. We
+        // do not want to stash the rewards to keep the cost low. However, using this method means
+        // that `_pendingRewards()` will change, hence a user might “lose” rewards earned since
+        // `_lastUpdate`. It is not a very big deal as the `_lastUpdate` is likely to be updated
+        // frequently, but just something to acknowledge.
+        uint256 newRewardRate;
+        if (_lastUpdate >= _periodFinish) {
+            assembly {
+                newRewardRate := div(amount, tmpPeriodDuration)
+            }
+        } else {
+            unchecked {
+                uint256 leftover = (_periodFinish - _lastUpdate) * _rewardRate;
+                assembly {
+                    newRewardRate := div(add(amount, leftover), tmpPeriodDuration)
+                }
+            }
+        }
+        if (newRewardRate == 0) revert InvalidRewardRate(newRewardRate);
+        _rewardRate = uint80(newRewardRate);
+        unchecked {
+            _lastUpdate = uint40(block.timestamp);
+            _periodFinish = uint40(block.timestamp + tmpPeriodDuration);
+        }
+        _transferFromCaller(amount);
+        emit RewardAdded(amount);
+    }
+
+    function rewardRate() public view returns (uint256) {
+        return _periodFinish < block.timestamp ? 0 : _rewardRate;
+    }
+
+    function periodFinish() public view returns (uint256) {
+        return _periodFinish;
+    }
+
+    function lastUpdate() public view returns (uint256) {
+        return _lastUpdate;
+    }
+
+    function totalRewardAdded() public view returns (uint256) {
+        return _totalRewardAdded;
+    }
+
+    function _claim() internal returns (uint256 reward) {
+        reward = _pendingRewards();
+        _lastUpdate = uint40(block.timestamp);
+    }
+
+    function _transferToCaller(uint256 amount) internal {
+        if (!rewardsToken.transfer(msg.sender, amount)) {
+            revert FailedTransferTo(msg.sender, amount);
+        }
+    }
+
+    function _transferFromCaller(uint256 amount) internal {
+        if (!rewardsToken.transferFrom(msg.sender, address(this), amount)) {
+            revert FailedTransferFrom(msg.sender, amount);
+        }
+    }
+
+    function _pendingRewards() internal view returns (uint256 rewards) {
+        uint256 tmpPeriodFinish = _periodFinish;
+        uint256 lastTimeRewardApplicable =
+            tmpPeriodFinish < block.timestamp ? tmpPeriodFinish : block.timestamp;
+        uint256 tmpLastUpdate = _lastUpdate;
+        if (lastTimeRewardApplicable > tmpLastUpdate) {
+            unchecked {
+                rewards = (lastTimeRewardApplicable - tmpLastUpdate) * _rewardRate;
+            }
+        }
+    }
+}
