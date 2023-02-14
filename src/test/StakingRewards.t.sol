72be348f5a460385f4bbf99f41ea0a42e576091c
Sunur Efe Vural <efe@efe.kim>
2023-02-14 20:20:56 +0300

    update test using bound

--- src/test/StakingRewards.t.sol
+++ src/test/StakingRewards.t.sol
@@ -16,27 +16,30 @@ contract StakingRewardsTests is Test {
         IERC20(rewardsToken).approve(address(stakingRewards), type(uint256).max);
     }
 
-    function testStake(uint256 amount) public {
-        if (amount > type(uint96).max) {
-            vm.expectRevert(
-                abi.encodeWithSelector(StakingRewards.InvalidStakeAmount.selector, amount)
-            );
-            stakingRewards.stake(amount);
-        } else {
-            uint160 rewardPerTokenStored = stakingRewards.rewardPerTokenStored();
-
-            stakingRewards.stake(amount);
-
-            (uint160 rewardPerTokenPaid, uint96 balance) = stakingRewards.users(address(this));
-            assertEq(rewardPerTokenStored, rewardPerTokenPaid);
-            assertEq(amount, balance);
-            assertEq(IERC20(rewardsToken).balanceOf(address(this)), type(uint256).max - amount);
-            assertEq(stakingRewards.totalStaked(), amount);
-        }
+    function test_RevertWhen_StakeOverflow(uint256 amount) public {
+        amount = bound(amount, type(uint96).max + 1, type(uint256).max);
+        vm.expectRevert(
+            abi.encodeWithSelector(StakingRewards.InvalidStakeAmount.selector, amount)
+        );
+        stakingRewards.stake(amount);
     }
 
-    function testWithdraw(uint96 stakeAmount, uint96 withdrawAmount) public {
-        testStake(stakeAmount);
+    function test_Stake(uint256 amount) public {
+        amount = bound(amount, 0, type(uint96).max);
+
+        uint160 rewardPerTokenStored = stakingRewards.rewardPerTokenStored();
+
+        stakingRewards.stake(amount);
+
+        (uint160 rewardPerTokenPaid, uint96 balance) = stakingRewards.users(address(this));
+        assertEq(rewardPerTokenStored, rewardPerTokenPaid);
+        assertEq(amount, balance);
+        assertEq(IERC20(rewardsToken).balanceOf(address(this)), type(uint256).max - amount);
+        assertEq(stakingRewards.totalStaked(), amount);
+    }
+
+    function test_Withdraw(uint96 stakeAmount, uint96 withdrawAmount) public {
+        test_Stake(stakeAmount);
 
         if (stakeAmount < withdrawAmount) {
             vm.expectRevert(
